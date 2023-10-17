3064730682006561e5c753ffb23f6f2be42e6e01
Nick Taylor <nick.taylor2013@yahoo.com>
2023-10-16 21:57:17 -0600

    add the ability to place the pieces
    Still a work in progress looks bad as of now and doenst work 100%

---
+++ ui/constants.py
@@ -0,0 +1,16 @@
+import pygame
+
+WIDTH, HEIGHT = 600, 600
+
+ROWS, COLS = 19,19
+LINE_WIDTH = 2
+SQUARE_SIZE = WIDTH//COLS
+CELL_SIZE = 540/18
+NUM_LINES = 19 # the standard size of a Go board
+# color define
+RED = (255,0,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+BLUE = (0,0,255)
+BACKGROUND = (227, 195, 107)
+GRAY = (128, 128, 128)
--- ui/game_display.py
+++ ui/game_display.py
@@ -1,11 +1,8 @@
 import pygame
-
+from board import Board
+from constants import BLACK,WHITE,GRAY,BACKGROUND,CELL_SIZE,NUM_LINES,ROWS,COLS
 class GameDisplay:
-    # Define some colors
-    BLACK = (0, 0, 0)
-    BACKGROUND = (227, 195, 107)
-    GRAY = (128, 128, 128)
-
+    
     def __init__(self) -> None:
         # Define constants
         self.NUM_LINES = 18
@@ -16,12 +13,17 @@ class GameDisplay:
 
         self.pixel_size = 540
         self.cell_size = self.pixel_size // self.NUM_LINES
-        
+        self.board = Board(self.cell_size)
 
         # Height of the button section
         
-
         self.set_up_pygame()
+        
+    def get_row_col_from_mouse(self, pos):
+        x, y = pos
+        row = y // self.cell_size
+        col = x // self.cell_size
+        return row, col
 
     def set_up_pygame(self):
         # Initialize Pygame
@@ -38,12 +40,27 @@ class GameDisplay:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                elif event.type == pygame.VIDEORESIZE:
+                if event.type == pygame.VIDEORESIZE:
                     self.pixel_size = self.get_valid_screen_size(event.size)
                     self.cell_size = (self.pixel_size // self.NUM_LINES) - (2 * self.BORDER_SIZE // self.NUM_LINES)
                     self.window_height = self.pixel_size + self.CONTROLS_HEIGHT + (1.5 * self.SAFE_AREA)
                     self.screen = pygame.display.set_mode((self.pixel_size + self.SAFE_AREA, self.window_height), pygame.RESIZABLE)
-
+                    
+                # this is for mouse click
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # get the location of the mouse click
+                    row, col = self.get_row_col_from_mouse(pygame.mouse.get_pos())
+                    # get the piece at that location
+                    piece = self.board.get_piece(row,col)
+                    # move the piece
+                    self.board.move(piece)
+                    
+                if event.type == pygame.MOUSEMOTION:
+                    #row, col = self.get_row_col_from_mouse(pygame.mouse.get_pos())
+                    #piece = self.board.get_piece(row,col)
+                    pass
+                    
+                
                 self.update_screen()
 
         # Quit Pygame
@@ -58,11 +75,11 @@ class GameDisplay:
     def update_screen(self):
         # Create a background surface and fill it with the background color
         background_surface = pygame.Surface((self.pixel_size + self.SAFE_AREA, self.window_height))
-        background_surface.fill(self.BACKGROUND)
+        background_surface.fill(BACKGROUND)
 
         # Create a controls surface
         controls_surface = pygame.Surface((self.pixel_size, self.CONTROLS_HEIGHT))
-        controls_surface.fill(self.BLACK)
+        controls_surface.fill(BLACK)
 
         # Draw buttons or other elements on the control surface
         # For example:
@@ -70,12 +87,12 @@ class GameDisplay:
 
         # Create a board surface and fill it with the background color
         board_surface = pygame.Surface((self.pixel_size, self.pixel_size))
-        board_surface.fill(self.BACKGROUND)
+        board_surface.fill(BACKGROUND)
 
         for i in range(self.NUM_LINES):
-            pygame.draw.line(board_surface, self.BLACK, (self.cell_size * i, 0),
+            pygame.draw.line(board_surface, BLACK, (self.cell_size * i, 0),
                              (self.cell_size * i, self.pixel_size), self.GRID_LINE_WIDTH)
-            pygame.draw.line(board_surface, self.BLACK, (0, self.cell_size * i),
+            pygame.draw.line(board_surface, BLACK, (0, self.cell_size * i),
                              (self.pixel_size, self.cell_size * i), self.GRID_LINE_WIDTH)
 
         # Star points
@@ -83,15 +100,32 @@ class GameDisplay:
                        (15, 9), (3, 15), (9, 15), (15, 15)]
         for point in star_points:
             pygame.draw.circle(
-                board_surface, self.BLACK, (self.cell_size * point[0], self.cell_size * point[1]), 6)
+                board_surface, BLACK, (self.cell_size * point[0], self.cell_size * point[1]), 6)
 
         # Draw a rim around the game board
-        pygame.draw.rect(board_surface, self.BLACK, (0, 0, self.pixel_size, self.pixel_size), self.BORDER_SIZE)
+        pygame.draw.rect(board_surface, BLACK, (0, 0, self.pixel_size, self.pixel_size), self.BORDER_SIZE)
 
         # Blit the background and board surfaces on the main screen
         self.screen.blit(background_surface, (0, 0))
         self.screen.blit(controls_surface, (self.SAFE_AREA // 2, self.pixel_size + self.SAFE_AREA))
         self.screen.blit(board_surface, (self.SAFE_AREA // 2, self.SAFE_AREA // 2))
+        
+        self.board.draw(self.screen,self.cell_size)
+        
+        white_left,black_left = self.board.get_pieces_left()
+        turn = self.board.get_turn()
+        
+        # draw the text to display the number of pieces left
+        
+        self.screen.blit(pygame.font.SysFont('Arial', 20).render('White pieces left: ' + str(white_left), True, WHITE), (self.SAFE_AREA // 2, self.pixel_size + self.SAFE_AREA + 20))
+        self.screen.blit(pygame.font.SysFont('Arial', 20).render('Black pieces left: ' + str(black_left), True, WHITE), (self.SAFE_AREA // 2, self.pixel_size + self.SAFE_AREA + 40))
+        
+        # draw the text to display whose turn it is
+        
+        if(turn % 2 == 0):
+            self.screen.blit(pygame.font.SysFont('Arial', 20).render('Black\'s turn', True, WHITE), (self.SAFE_AREA // 2, self.pixel_size + self.SAFE_AREA + 60))
+        else:
+            self.screen.blit(pygame.font.SysFont('Arial', 20).render('White\'s turn', True, WHITE), (self.SAFE_AREA // 2, self.pixel_size + self.SAFE_AREA + 60))
 
         # Update the screen
         pygame.display.flip()
