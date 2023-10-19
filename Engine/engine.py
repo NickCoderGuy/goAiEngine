50bb3ab3d7eba6c5af83774beb5200aabde6b738
Suav <thoskisson@outlook.com>
2023-10-19 13:26:14 -0600

    basic logic

---
+++ Engine/engine.py
@@ -0,0 +1,32 @@
+from constants import *
+
+turn_counter = BLACK
+pass_counter = 0
+board = [[[EMPTY] * LENGTH] * LENGTH]
+
+def newGame():
+    global turn_counter, pass_counter, board
+    turn_counter = BLACK
+    pass_counter = 0
+    board = [[[EMPTY] * LENGTH] * LENGTH]
+
+def changeTurn():
+    global turn_counter
+    if turn_counter == BLACK:
+        turn_counter = WHITE
+    else:
+        turn_counter = BLACK
+def passTurn():
+    global turn_counter, pass_counter
+    changeTurn()
+    pass_counter += 1
+
+def resign():
+
+
+def placePiece(x, y):
+    global turn_counter, pass_counter, board
+    if board[x][y] == EMPTY:
+        board[x][y] = turn_counter
+        pass_counter = 0
+        changeTurn()
