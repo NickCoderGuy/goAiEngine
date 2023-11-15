47a2e8f58bca8bebe1743726532c6c8bda60419b
JJStephens <siahstephens@gmail.com>
2023-11-15 14:38:06 -0700

    added menu

--- ui/game_display.py
+++ ui/game_display.py
@@ -1,4 +1,4 @@
-from ui.constants import BLACK, WHITE, BACKGROUND
+from ui.constants import BLACK, WHITE, BACKGROUND, DARK_GRAY
 
 
 class GameDisplay:
@@ -17,6 +17,11 @@ class GameDisplay:
         self.cell_size = self.pixel_size // self.NUM_LINES
         self.board_padding = self.cell_size
 
+        self.window_height = 0
+        self.window_width = 0
+
+        self.current_screen = "main_menu"
+
         self.set_up_pygame()
 
     def get_row_col_from_mouse(self, pos):
@@ -33,30 +38,37 @@ class GameDisplay:
         # Calculate the total height of the window (including the button section)
         self.window_height = self.pixel_size + \
             self.CONTROLS_HEIGHT + (1.5 * self.SAFE_AREA)
-        self.screen =self.pygame.display.set_mode(
-            (self.pixel_size + self.SAFE_AREA + self.board_padding * 2, self.window_height),self.pygame.RESIZABLE)
+        self.window_width = self.pixel_size + self.SAFE_AREA + self.board_padding * 2
+        self.screen =self.pygame.display.set_mode((self.window_width, self.window_height),self.pygame.RESIZABLE)
         
         initial_go_board_state = [[0 for _ in range(self.NUM_LINES + 1)] for _ in range(self.NUM_LINES + 1)]
 
-        self.display_board(initial_go_board_state)
+        # self.display_board(initial_go_board_state)
+        self.display_main_menu()
 
 
     def get_valid_screen_size(self, new_screen_size):
         # Round to the nearest multiple of NUM_LINES
-        new_screen_size = ((new_screen_size[0] - self.SAFE_AREA) // self.NUM_LINES * self.NUM_LINES,
-                           (new_screen_size[1] - self.SAFE_AREA) // self.NUM_LINES * self.NUM_LINES)
+        new_screen_size = ((new_screen_size[0] - (self.SAFE_AREA + self.board_padding)) // self.NUM_LINES * self.NUM_LINES,
+                           (new_screen_size[1] - (self.SAFE_AREA + self.board_padding)) // self.NUM_LINES * self.NUM_LINES)
         return max(432, min(new_screen_size[0], new_screen_size[1]))
     
     def resize(self, new_screen_size):
+        print(f"new screen size: {new_screen_size}")
         self.pixel_size = self.get_valid_screen_size(new_screen_size)
+        print(f"pixel size: {self.pixel_size}")
         self.cell_size = (
             self.pixel_size // self.NUM_LINES) - (2 * self.BORDER_SIZE // self.NUM_LINES)
         self.window_height = self.pixel_size + \
             self.CONTROLS_HEIGHT + (1.5 * self.SAFE_AREA)
+        self.window_width = self.pixel_size + self.SAFE_AREA + 2 * self.board_padding
         self.board_padding = self.cell_size
-        self.screen = self.pygame.display.set_mode(
-            (self.pixel_size + self.SAFE_AREA + 2 * self.board_padding, self.window_height), self.pygame.RESIZABLE)
-        self.display_board(self.pieces_array)
+        self.screen = self.pygame.display.set_mode((self.window_width, self.window_height), self.pygame.RESIZABLE)
+
+        if self.current_screen == "main_menu":
+            self.display_main_menu()
+        else:
+            self.display_board(self.pieces_array)
 
     def display_board(self, new_pieces_array):
         self.pieces_array = new_pieces_array
@@ -76,7 +88,7 @@ class GameDisplay:
 
         # Create a board surface and fill it with the background color
 
-        self.board_padding = self.cell_size
+        # self.board_padding = self.cell_size
 
         board_surface = self.pygame.Surface((self.pixel_size + 2 * self.board_padding, self.pixel_size + 2 * self.board_padding))
         board_surface.fill(BACKGROUND)
@@ -117,3 +129,45 @@ class GameDisplay:
 
         # Update the screen
         self.pygame.display.flip()
+
+
+    def display_main_menu(self):
+        self.pygame.display.set_caption("Go")
+        button_width = self.pixel_size // 2
+        button_height = 50
+        margin = self.window_width // 2 - button_width // 2
+
+        start_button_rect = self.pygame.Rect(margin, self.window_height // 2, button_width, button_height)
+        quit_button_rect = self.pygame.Rect(margin, self.window_height // 1.5, button_width, button_height)
+
+        hover_start = start_button_rect.collidepoint(self.pygame.mouse.get_pos())
+        hover_quit = quit_button_rect.collidepoint(self.pygame.mouse.get_pos())
+
+        # Draw the background
+        self.screen.fill(BACKGROUND)
+
+        # Draw buttons
+        self.draw_button("Start Game", start_button_rect, hover_start)
+        self.draw_button("Quit", quit_button_rect, hover_quit)
+
+        self.pygame.display.flip()
+        return hover_start, hover_quit
+
+    def draw_button(self, text, rect, hover):
+        # Define fonts
+
+        self.pygame.font.init()
+        font = self.pygame.font.SysFont(None, 36)
+        color = BLACK if not hover else DARK_GRAY
+        self.pygame.draw.rect(self.screen, color, rect)
+        
+        text_surface = font.render(text, True, WHITE)
+        text_rect = text_surface.get_rect(center=rect.center)
+        self.screen.blit(text_surface, text_rect)
+
+    def set_current_screen(self, screen):
+        # set to main_menu, board, or whatever other screen
+        self.current_screen = screen
+    
+    def get_current_screen(self):
+        return self.current_screen
