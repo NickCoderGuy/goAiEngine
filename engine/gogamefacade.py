6cf0f15884ffc98f7bfddd16480520fb7e60eb3f
Davis Forster <dp4ster007@me.com>
2023-11-18 18:47:11 -0700

    removed state as a parameter for facade

--- engine/gogamefacade.py
+++ engine/gogamefacade.py
@@ -1,20 +1,47 @@
 from engine import gogame, govars
 import numpy as np
+import json
 
 
-def new_game():
-    state = gogame.init_state(govars.SIZE)
-    return state
+class GoGameFacade():
+
+    def __init__(self) -> None:
+        self.state_history = []
+
+    def new_game(self):
+        state = gogame.init_state(govars.SIZE)
+        self.state_history = []
+        self.state_history.append(state)
+        return self.serialize(state)
 
 
-def pass_turn(state):
-    board_shape = state.shape[1:]
-    pass_idx = np.prod(board_shape)
-    new_state = gogame.next_state(state, pass_idx)
-    return new_state
+    def pass_turn(self):
+        state = self.get_current_board()
+        board_shape = state.shape[1:]
+        pass_idx = np.prod(board_shape)
+        new_state = gogame.next_state(state, pass_idx)
+
+        self.state_history.append(state)
+        return self.serialize(new_state)
 
 
-def make_turn(state, x, y):
-    position = ((x - 1) * govars.SIZE) + y
-    new_state = gogame.next_state(state, position)
-    return new_state
+    def make_move(self, x, y):
+        state = self.get_current_board()
+        position = ((x) * govars.SIZE) + y
+        new_state = gogame.next_state(state, position)
+
+        self.state_history.append(state)
+        return self.serialize(new_state)
+
+
+    def combine(self, black_board, white_board):
+        full_board = black_board + (white_board * 2)
+        return full_board
+
+    def serialize(self, state):
+        json_dict = {}
+        json_dict['board'] = self.combine(state[govars.BLACK], state[govars.WHITE])
+        json_dict['turn'] = gogame.turn(state)
+
+    def get_current_board(self):
+        return self.state_history[-1]
